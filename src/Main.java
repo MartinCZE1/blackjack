f065b95f43ca027244423f90e178ca8e72b0e45b
Martin <martinhercik16@gmail.com>
2023-05-22 15:19:07 +0200

    Mechanika hry, lízání karet

--- src/Main.java
+++ src/Main.java
@@ -5,47 +5,79 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
+
+        int numOfCards = 2;
+        int cardStartOne;
+        int cardStartTwo;
+
+        String end;
+
         System.out.println("Vítejte v casinu");
         System.out.println("Jsi starší 18-ti let? (ano/ne)");
         String age = sc.nextLine().trim().toLowerCase();
-//start
+
         switch (age) {
             case "ano" -> {
-                System.out.println("Kolik máš u sebe peněz");
-                int money = sc.nextInt();
+                System.out.println("Kolik peněz u sebe máš?");
+                double money = sc.nextDouble();
 
                 System.out.println("Kolik chceš vsadit?");
-                int bet = sc.nextInt();
+                double bet = sc.nextDouble();
 
-                int numOfCards;
-                int cardStartOne;
-                int cardStartTwo;
+                sc.nextLine();
 
-                if (money < bet) {
-                    System.out.println("Hodnota tvého mění nestačí na požadovanou sázku.");
-                } else if (bet == 0) {
-                    System.out.println("Musíte něco přeci vsadit");
-                } else if (money > bet) {
-                    cardStartOne = rd.nextInt(5);
+                if (money > bet) {
+                    cardStartOne = rd.nextInt(5) + 1;
                     System.out.println("Obdržel jsi první kartu s hodnotou: " + cardStartOne);
 
-                    cardStartTwo = rd.nextInt(5);
+                    cardStartTwo = rd.nextInt(5) + 1;
                     System.out.println("Obdržel jsi druhou kartu s hodnotou: " + cardStartTwo);
 
-                    System.out.println("Chcete opravdu zahájit hru? (ano/ne)");
+                    int cardValue = cardStartOne + cardStartTwo;
+
+                    System.out.println("Aktuální hodnota tvých karet: " + cardValue);
+                    System.out.println("Aktuální počet tvých karet " + numOfCards);
+
+                    System.out.println("Přejete si zahájit hru?");
                     String start = sc.nextLine().trim().toLowerCase();
 
-                    if (start.equals("ano")) {
-                        int cardValue = cardStartOne + cardStartTwo;
-                        numOfCards = 2;
-                        System.out.println("Aktuální hodnota tvých karet je: " + cardValue);
-                        System.out.println("Aktuální počet tvých karet je: " + numOfCards);
-                    } else {
-                        System.out.println("Tak snad příště. Mějte se.");
+                    switch (start) {
+                        case "ano" -> {
+                            System.out.println("Aktuální hodnota tvých karet je: " + cardValue);
+                            System.out.println("Aktuální počet tvých karet je: " + numOfCards);
+
+                            do {
+                                System.out.println("Vytáhl jste si kartu.");
+                                int newCard = rd.nextInt(7) + 1;
+                                cardValue += newCard;
+                                numOfCards++;
+
+                                System.out.println("Hodnota nové karty: " + newCard);
+
+                                System.out.println("Aktuální hodnota karet: " + cardValue);
+                                System.out.println("Aktuální počet karet: " + numOfCards);
+
+                                if (cardValue > 21) {
+                                    System.out.println("Bust");
+                                    end = "ne";
+                                } else if (cardValue == 21) {
+                                    System.out.println("Blackjack");
+                                    end = "ne";
+                                } else {
+                                    System.out.println("Chcete další kartu? (ano/ne)");
+                                    end = sc.nextLine();
+                                }
+                            } while (end.equals("ano"));
+                        }
+                        case "ne" -> System.out.println("Tak třeba příště.");
+
+                        default -> System.out.println("Mluvte zřetelně");
                     }
                 }
             }
-            case "ne" -> System.out.println("Sem mohou pouze osoby starší 18-ti let.");
+            case "ne" -> System.out.println("Sem mají přístup jen dospělí.");
+
+            default -> System.out.println("Mluvte zřetelně");
         }
     }
 }
